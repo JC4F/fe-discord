8f99b1bd826a7020eea61cae842c287671618f9a
JC4F <lequan12122002@gmail.com>
2023-04-16 20:53:15 +0700

    fake spinner login

--- src/page/login/qr/index.tsx
+++ src/page/login/qr/index.tsx
@@ -2,20 +2,36 @@ import React from "react";
 import { QRCode } from "react-qrcode-logo";
 import styles from "./index.module.css";
 import DiscordLogo from "assest/image/discord-logo.png";
+import { CircularProgress } from "@mui/material";
 
 const QRCodeWrapper: React.FC = () => {
   // Qr sễ lấy từ context xuống để render, socket, một khoảng thời gian thì hết hạn link
   // gọi api lấy link mới
 
+  // fake loading
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIsLoading(!isLoading);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [isLoading]);
+
   return (
     <div className={styles.qrWrapper}>
       <div className={styles.customQr}>
-        <QRCode
-          value="https://github.com/gcoro/react-qrcode-logo"
-          logoImage={DiscordLogo}
-          logoWidth={50}
-          logoHeight={50}
-        />
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <QRCode
+            value="https://github.com/gcoro/react-qrcode-logo"
+            logoImage={DiscordLogo}
+            logoWidth={50}
+            logoHeight={50}
+          />
+        )}
       </div>
       <h2>Dang nhap bang Ma QR</h2>
       <p>
