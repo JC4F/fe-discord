aef4b5cfd0b731e39af757701e20cbfab8f7a829
JC4F <lequan12122002@gmail.com>
2023-06-05 14:25:39 +0700

    update dialog search header

--- src/page/chanels/me/index.tsx
+++ src/page/chanels/me/index.tsx
@@ -1,15 +1,44 @@
 import React from "react";
 import MainLayout from "share/molecules/main-layout";
+import styles from "./index.module.css";
+import SearchChanelDialog from "share/dialog/search-chanel";
+
+interface IMeState {
+  dialogOpen: boolean;
+}
+
+const initMeState: IMeState = {
+  dialogOpen: false,
+};
 
 const Me: React.FC = () => {
-  console.log(2);
+  const [meState, setMeState] = React.useState<IMeState>(initMeState);
+
+  const handleHeadDialogFind = React.useCallback((makeOpen: boolean) => {
+    setMeState((pre) => ({
+      ...pre,
+      dialogOpen: makeOpen,
+    }));
+  }, []);
+
   return (
     <>
       <MainLayout
-        NextHeadEle={<div>next-head</div>}
+        NextHeadEle={
+          <button
+            onClick={() => handleHeadDialogFind(true)}
+            className={styles.customButtonHeadd}
+          >
+            Tìm hoặc bắt đầu một cuộc trò chuyện
+          </button>
+        }
         NextContentEle={<div>next-content</div>}
         MainHeadEle={<div>main-head</div>}
         MainContentEle={<div>main-content</div>}
+      />
+      <SearchChanelDialog
+        isOpen={meState.dialogOpen}
+        handleCloseDialog={handleHeadDialogFind}
       />
     </>
   );
---
+++ src/share/dialog/search-chanel/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Dialog, DialogContent } from "@mui/material";
+import styles from "./index.module.css";
+
+interface ISearchDialogProps {
+  isOpen: boolean;
+  handleCloseDialog: (makeOpen: boolean) => void;
+}
+
+const SearchChanelDialog: React.FC<ISearchDialogProps> = ({
+  isOpen,
+  handleCloseDialog,
+}) => {
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={() => handleCloseDialog(false)}
+      maxWidth="md"
+    >
+      <DialogContent
+        sx={{
+          width: "570px",
+          borderRadius: "8px",
+          padding: "20px 20px 0",
+          display: "flex",
+          flexDirection: "column",
+          color: "var(--text-normal)",
+          backgroundColor: "var(--background-secondary)",
+        }}
+      >
+        <input
+          className={styles.customInputDialog}
+          type="text"
+          placeholder="Bạn muốn đi đâu?"
+        />
+        {/* <div className={styles.haveResult}></div> */}
+        <div className={styles.notFoundResult}>
+          <div className={styles.notFoundText}>
+            Không thể tìm thấy những thứ bạn quan tâm?
+          </div>
+        </div>
+        <div className={styles.tip}>Tìm kiếm với @ # để lọc bớt kết quả.</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default SearchChanelDialog;
